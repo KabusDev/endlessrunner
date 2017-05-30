a97ff9956a1686e139bf0eabd3a66e376ea64c86
Kabus <kabusiblis@gmail.com>
2017-05-30 09:36:07 +0100

    Added Primitive Obstacles
    no collisions yet
    TODO: Collisions, Generation, Pause untill game button is pressed.

--- main_all.py
+++ main_all.py
@@ -1,6 +1,6 @@
 import pygame
 import sys
-import time
+# import time
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -11,6 +11,7 @@ pygame.font.init()
 menu_check = True
 pause_check = False
 fps_check = False
+
 
 class ScrRes:
     def __init__(self, x, y):
@@ -28,10 +29,11 @@ res_var = (res_x, res_y)
 
 scale_var = (res_x * res_y / 10000)/2
 res_scale = round(scale_var)
-print(res_scale) # hack way of getting scaling for text?
+print(res_scale)  # hack way of getting scaling for text?
 
 resolution = res_var  # this will change in settings
 screen = pygame.display.set_mode(resolution, pygame.HWSURFACE)
+# wont be used but might as well leave it here since it would take a while to change stuff back to res co-ords
 
 
 class RGB:  # class for colors
@@ -57,6 +59,123 @@ scr_button_1 = Rect(screen, RGB.blue, (res_x/4, res_y/8, res_x/2, res_y/5))
 scr_button_2 = Rect(screen, RGB.black, (res_x/4, res_y/1.5, res_x/2, res_y/5))
 scr_overlay = Rect(screen, RGB.black_alpha, (0, 0, res_x, res_y))
 level_box = Rect(screen, RGB.black, (0, res_y/1.25, res_x, res_y))
+
+
+def collision_detect(self, sprite):
+    return self.rect.colliderect(sprite.rect)
+
+
+class Player():
+    is_jumping = False
+    # self.loc_x = round(res_x / 7)
+    # self.loc_y = round(res_y / 1.31)
+    loc_x = 100
+    loc_y = 450
+    force = 8
+    mass = 2
+
+    def jump(self):
+        print("ply jump")  # debug
+        self.is_jumping = True  # jump trigger
+
+    def logic_update(self):
+        # global f
+        # time.sleep(0.5) # debug timer for prints
+
+        # force calculation 0.5 * mass * velocity^2.
+        if self.is_jumping is True:
+            if self.force >= 0:
+                f = -(0.5 * self.mass * (self.force * self.force))
+            else:
+                f = (0.5 * self.mass * (self.force * self.force))
+            self.loc_y += f
+            self.force -= 1
+
+            if self.loc_y >= 450:
+                self.loc_y = 450
+                self.is_jumping = False
+                self.force = 8
+
+            # print(f)
+            # print("force ", self.force)
+            # print("loc ", self.loc_y)
+
+
+class WorldGen:
+
+    # todo del from file
+    # shifting the world
+    # use predefined obstacle patterns and apply patterns procedurally?
+    # todo need function for detecting collisions between ply and obstacles
+    # todo game over screen
+    # todo pause state so that game only runs after starting to play
+
+    def __init__(self):
+        self.obstacle_group = pygame.sprite.Group()
+        self.powerup_group = pygame.sprite.Group()  # low priority
+        self.world_shift = 0  # use this to move the world instead of blocks independently?
+        self.x_1 = 400
+        self.x_2 = 700
+        self.x_3 = 1100
+        self.speed_var = 10 # this will be used to change acceleration speed of obstacles
+
+    def obstacle_obj(self):
+
+        triangle = pygame.draw.polygon(screen, RGB.blue,
+                                      (((self.x_1-50), res_y / 1.25),
+                                       (self.x_1, res_y / 1.5),
+                                       ((self.x_1+50), res_y / 1.25)))
+
+        triangle2 = pygame.draw.polygon(screen, RGB.blue,
+                                        (((self.x_2 - 50), res_y / 1.25),
+                                         (self.x_2, res_y / 1.5),
+                                         ((self.x_2 + 50), res_y / 1.25)))
+
+        triangle3 = pygame.draw.polygon(screen, RGB.blue,
+                                        (((self.x_3 - 50), res_y / 1.25),
+                                         (self.x_3, res_y / 1.5),
+                                         ((self.x_3 + 50), res_y / 1.25)))
+
+
+
+    # def world_shifter(self, shift_x):  # func for global shift in items, player is exempt
+    #     self.world_shift += shift_x  # global shifting, something
+    #
+    #     for obstacle in self.obstacle_group:
+    #         obstacle.rect.x += shift_x
+
+        # todo powerup stuff later
+        pass
+
+    def world_update(self):
+        # print (self.x_1)
+
+        self.x_1 -= self.speed_var
+        self.x_2 -= self.speed_var
+        self.x_3 -= self.speed_var
+
+        if self.x_1*1.2 <= -250:
+            self.x_1 = 850
+        if self.x_2*1.2 <= -250:
+            self.x_2 = 850
+        if self.x_3*1.2 <= -250:
+            self.x_3 = 850
+        pass
+    pass
+
+
+class Obstacles():
+    # do polygons that change on x axis for horizontal movement to simulate level progression?
+    # y should be static, x should be dynamically changed, use modifier from one int var?
+
+    # x as centre, (x*.5,res_y/1.25), (x,res_y/1.5), (x*1.5,res_y/1.25) ?
+    # x would have to have about 50~ pixels in between?, should be equilateral
+    # something like this for placements of obstacles
+    pass
+
+
+ply = Player()
+world = WorldGen()
 
 
 def main_menu():
@@ -90,68 +209,10 @@ def pause_menu():
 
 
 def game_screen():
-    global bounds
     player_ball = pygame.draw.circle(screen, RGB.black, (round(ply.loc_x), round(ply.loc_y)), 30)
-    # pygame.draw.rect(screen, RGB.blue, (0, 0, 20, 20))  # debug box
-    bounds = pygame.draw.rect(level_box.surface, level_box.color, level_box.xy_loc) # constant
+    bounds = pygame.draw.rect(level_box.surface, level_box.color, level_box.xy_loc)  # constant
+    world.obstacle_obj()
     pass
-
-
-def collision_detect(self, sprite):
-    return self.rect.colliderect(sprite.rect)
-
-
-class Player():
-    is_jumping = False
-    # self.loc_x = round(res_x / 7)
-    # self.loc_y = round(res_y / 1.31)
-    loc_x = 100
-    loc_y = 450
-    force = 8
-    mass = 2
-
-    def jump(self):
-        print("ply jump")  # debug
-        self.is_jumping = True  # jump trigger
-
-    def logic_update(self):
-        global f
-        # time.sleep(0.5) # debug timer for prints
-
-        # force calculation 0.5 * mass * velocity^2.
-        if self.is_jumping is True:
-            if self.force >= 0:
-                f = -(0.5 * self.mass * (self.force * self.force))
-            else:
-                f = (0.5 * self.mass * (self.force * self.force))
-            self.loc_y = self.loc_y + f
-            self.force = self.force - 1
-
-            if self.loc_y >= 450:
-                self.loc_y = 450
-                self.is_jumping = False
-                self.force = 8
-
-            # print(f)
-            # print("force ", self.force)
-            # print("loc ", self.loc_y)
-
-
-class WorldGen:
-    # shifting the world
-    # use predefined obstacle patterns and apply patterns procedurally
-    # todo need function for detecting collisions between ply and obstacles
-    # todo gameover screen
-
-
-    pass
-
-
-class Obstacles():
-    pass
-
-
-ply = Player()
 
 
 def controls():
@@ -229,6 +290,7 @@ def ui_start_game():
 
 
 def update():
+    world.world_update()
     ply.logic_update()
     controls()
     render(screen)
@@ -242,4 +304,4 @@ while True:
     fps_counter = clock.get_fps()
     # print(fps_counter)
     update()
-    screen.fill(RGB.white)  # redundant fill just incase ui doesnt change background fill of screen colour
+    screen.fill(RGB.white)  # redundant fill just in case ui doesnt change background fill of screen colour
