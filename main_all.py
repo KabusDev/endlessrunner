36cd8d6f51077339c73487059a70d1d7688f06fb
Kabus <kabusiblis@gmail.com>
2017-06-01 09:44:37 +0100

    Added GameOver Screen

--- main_all.py
+++ main_all.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import random
 # import time
 
 pygame.init()
@@ -12,7 +13,7 @@ menu_check = True
 pause_check = False
 ply_dead = False
 fps_check = False
-
+reset_bool = False
 
 class ScrRes:
     def __init__(self, x, y):
@@ -40,7 +41,11 @@ class RGB:  # class for colors
     white = (255, 255, 255)
     black = (0, 0, 0)
     blue = (0, 0, 255)
-    red = (255, 0, 0)
+
+    green = (105, 242, 92)
+    yellow = (237, 242, 92)
+    red = (242, 92, 92)
+
     black_alpha = (0, 0, 0, 65) # alpha doesnt work
     grey = (178, 178, 178)
 
@@ -59,10 +64,6 @@ scr_button_1 = Rect(screen, RGB.blue, (res_x/4, res_y/8, res_x/2, res_y/5))
 scr_button_2 = Rect(screen, RGB.black, (res_x/4, res_y/1.5, res_x/2, res_y/5))
 scr_overlay = Rect(screen, RGB.black_alpha, (0, 0, res_x, res_y))
 level_box = Rect(screen, RGB.black, (0, res_y/1.25, res_x, res_y))
-
-
-def collision_detect(self, sprite):
-    return self.rect.colliderect(sprite.rect)
 
 
 class Player:
@@ -96,33 +97,25 @@ class Player:
                 self.is_jumping = False
                 self.force = 8
 
-            # print(f)
-            # print("force ", self.force)
-            # print("loc ", self.loc_y)
-
 
 class WorldGen:
     # shifting the world
     # use predefined obstacle patterns and apply patterns procedurally?
     # todo need function for detecting collisions between ply and obstacles
     # todo game over screen
-    # todo pause state so that game only runs after starting to play
 
     def __init__(self):
         self.obstacle_group = pygame.sprite.Group()
         self.powerup_group = pygame.sprite.Group()  # low priority
         self.world_shift = 0  # use this to move the world instead of blocks independently?
-        self.x_1 = 400
-        self.x_2 = 700
-        self.x_3 = 1100
+        self.x_1 = 800
+        self.x_2 = 1100
+        self.x_3 = 1500 # initial spawns, off screen
         # these will get modified for procedural gen
         self.speed_var = 10 # this will be used to change acceleration speed of obstacles
 
-    def procedural_gen(self):
-        pass
-
     def obstacle_obj(self):
-        global triangle, triangle2, triangle3
+        global triangle, triangle2, triangle3, ply_dead
 
         triangle = pygame.draw.polygon(
             screen, RGB.blue,
@@ -146,25 +139,17 @@ class WorldGen:
         )
 
         if triangle.collidepoint(ply.loc_x, ply.loc_y):
-            print("triangle 1 collision")
+            ply_dead = True
         if triangle2.collidepoint(ply.loc_x, ply.loc_y):
-            print("triangle 2 collision")
+            ply_dead = True
         if triangle3.collidepoint(ply.loc_x, ply.loc_y):
-            print("triangle 3 collision")
-
-    # def world_shifter(self, shift_x):  # func for global shift in items, player is exempt
-    #     self.world_shift += shift_x  # global shifting, something
-    #
-    #     for obstacle in self.obstacle_group:
-    #         obstacle.rect.x += shift_x
-
-
+            ply_dead = True
         pass
 
     # todo power up stuff later
 
     def world_update(self):
-        global triangle
+        global start_x_1, start_x_2, start_x_3
         # print (self.x_1)
 
         self.x_1 -= self.speed_var
@@ -172,15 +157,21 @@ class WorldGen:
         self.x_3 -= self.speed_var
 
         end_x = -250
-        start_x = 850
+        start_x_1 = 850
+        start_x_2 = 850
+        start_x_3 = 850
 
         if self.x_1*1.2 <= end_x:
-            self.x_1 = start_x
+            self.x_1 = start_x_1
         if self.x_2*1.2 <= end_x:
-            self.x_2 = start_x
+            self.x_2 = start_x_2
         if self.x_3*1.2 <= end_x:
-            self.x_3 = start_x
-        # can this be better?
+            self.x_3 = start_x_3
+
+    def reset(self):
+        self.x_1 = 800
+        self.x_2 = 1100
+        self.x_3 = 1500
         pass
     pass
 
@@ -222,19 +213,21 @@ def pause_menu():
 def game_screen():
     player_ball = pygame.draw.circle(screen, RGB.black, (round(ply.loc_x), round(ply.loc_y)), 30)
     bounds = pygame.draw.rect(level_box.surface, level_box.color, level_box.xy_loc)  # constant
+    # hp_bar = pygame.draw.rect(screen, RGB.green, )
     world.obstacle_obj()
     pass
 
 
 def game_over():
+    global reset_button, menu_button2
     background_menu = pygame.draw.rect(scr_overlay.surface, scr_overlay.color, scr_overlay.xy_loc)
-    continue_button = pygame.draw.rect(scr_button_1.surface, scr_button_1.color, scr_button_1.xy_loc)
-    menu_button = pygame.draw.rect(scr_button_2.surface, scr_button_1.color, scr_button_2.xy_loc)
+    reset_button = pygame.draw.rect(scr_button_1.surface, scr_button_1.color, scr_button_1.xy_loc)
+    menu_button2 = pygame.draw.rect(scr_button_2.surface, scr_button_1.color, scr_button_2.xy_loc)
 
-    continue_txt = Font_Basic.render("Restart", 1, RGB.white, None)
+    reset_txt = Font_Basic.render("Retry", 1, RGB.white, None)
     menu_txt = Font_Basic.render("Quit to Menu", 1, RGB.red, None)
 
-    screen.blit(continue_txt, (res_x / 2.5, res_y / 5.5))
+    screen.blit(reset_txt, (res_x / 2.5, res_y / 5.5))
     screen.blit(menu_txt, (res_x / 2.8, res_y / 1.375))
     # this is just a clone of the pause screen,
     pass
@@ -243,7 +236,7 @@ def game_over():
 def controls():
     for event in pygame.event.get():
         pygame.key.get_pressed()
-        global menu_check, pause_check
+        global menu_check, pause_check, ply_dead
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
@@ -271,6 +264,7 @@ def controls():
             if click == 1:
                 if menu_check is True:
                     if play_button.collidepoint(mouse_pos):  # pycharm whining, still functions correctly
+                        world.reset()
                         ui_start_game()
                         print("start")
                     if quit_button.collidepoint(mouse_pos):
@@ -282,6 +276,14 @@ def controls():
                         print("test")
                         menu_check = True
                         pause_check = False
+                if ply_dead is True:
+                    world.reset()
+                    if reset_button.collidepoint(mouse_pos):
+                        ply_dead = False
+                    if menu_button2.collidepoint(mouse_pos):
+                        menu_check = True
+                        ply_dead = False
+
                 else:
                     pass
         if event.type == pygame.QUIT:
@@ -323,6 +325,8 @@ def game_pause():
     if pause_check is False:
         world.speed_var = 10
         # will replace this
+        # todo correct restart of speed when modified
+        # todo freeze player jump when in pause
         pass
 
 
